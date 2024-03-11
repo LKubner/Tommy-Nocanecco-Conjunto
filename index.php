66905672d1ea5a9eaf349be7ff78726473ea6a80
Martín Silveira de Almeida <martin.2022311000@aluno.iffar.edu.br>
2024-03-11 16:38:36 -0300

    form cadastrar

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lista de compras</h1>
+    <h2>Menu</h2>
+    <ul>
+    <li><a href="formcad.html">Inserir produto</a> </li> <br>
+    <li><a href="formedit.html">Atualizar produto</a> </li> <br>
+    <li><a href="listar_produto.php">Listar produto</a> </li> <br>
+    <li><a href="formexcluir.html">Excluir produto</a> </li> <br>
+    </ul>
+</body>
+</html>
